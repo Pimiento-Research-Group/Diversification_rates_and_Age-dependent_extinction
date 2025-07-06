b4b12e779b54733818047bee4f9f701541bfe1e5
Kristina-K12 <kocakova.kristina@gmail.com>
2025-07-06 12:36:49 +0200

    plotting of preservation from terminal

--- code/3.2.process_pyrate_output.sh
+++ code/3.2.process_pyrate_output.sh
@@ -5,7 +5,7 @@ Processing of PyRate .log outputs
 1. Combining 10 replicates into a single set of posterior values
 2. Generating of the input file for plotting of the rates
 3. Generating a file containing estimated speciation and extinction times per taxon
-4. Generate file containing the preservation rate through time
+4. Generate file containing the preservation rate through time and plot it
 '
 #1.
 python3 ./PyRate-master/PyRate.py -combLogRJ /path_to_output_folder/
@@ -14,7 +14,9 @@ python3 ./PyRate-master/PyRate.py -plotRJ /path_to_output_folder/ -tag combined 
 #3.
 python3 ./PyRate-master/PyRate.py -ginput /path_to_output_folder/combined_10_mcmc.log
 #4
-python3 ./PyRate-master/PyRate.py -plotQ /path_to_output_folder/combined_10_mcmc.log -qShift /path_to_input_files/stages.txt -b 0.1
+python3 ./PyRate-master/PyRate.py -plotQ /path_to_output_folder/species_1_Grj_mcmc.log -qShift /path_to_input_files/stages.txt -b 0.1
+# to plot
+Rscript /path_to_output_folder/species_1_Grj_mcmc_RTT_Qrates.r
 
 
 
